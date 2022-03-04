ef8a8d2f75cb6fda5b3c42362f25e475ae5f8566
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2022-03-04 14:44:05 +0100

    Detour Unity Shader Find call to RoR2 LegacyShaderAPI

---
+++ RoR2BepInExPack/LegacyShaderDetours.cs
@@ -0,0 +1,52 @@
+﻿using MonoMod.RuntimeDetour;
+using RoR2;
+using System.Reflection;
+using UnityEngine;
+
+namespace RoR2BepInExPack;
+
+internal static class LegacyShaderDetours
+{
+    private static NativeDetour _shaderFindDetour;
+    private delegate Shader ShaderFindDefinition(string path);
+    private static ShaderFindDefinition _origFind;
+
+    internal static void Init()
+    {
+        const BindingFlags allFlags = (BindingFlags)(-1);
+
+        var shaderFindDetourConfig = new NativeDetourConfig { ManualApply = true };
+        _shaderFindDetour = new NativeDetour(
+                typeof(Shader).GetMethod(nameof(Shader.Find), allFlags, null, new[] { typeof(string) }, null),
+                typeof(LegacyShaderDetours).GetMethod(nameof(OnShaderFind), allFlags),
+                shaderFindDetourConfig
+            );
+        _origFind = _shaderFindDetour.GenerateTrampoline<ShaderFindDefinition>();
+    }
+
+    internal static void Enable()
+    {
+        _shaderFindDetour.Apply();
+    }
+
+    internal static void Disable()
+    {
+        _shaderFindDetour.Undo();
+    }
+
+    internal static void Destroy()
+    {
+        _shaderFindDetour.Free();
+    }
+
+    private static Shader OnShaderFind(string path)
+    {
+        var shader = LegacyShaderAPI.Find(path);
+        if (shader)
+        {
+            return shader;
+        }
+
+        return _origFind(path);
+    }
+}
--- RoR2BepInExPack/RoR2BepInExPack.cs
+++ RoR2BepInExPack/RoR2BepInExPack.cs
@@ -14,20 +14,24 @@ public class RoR2BepInExPack : BaseUnityPlugin
         Log.Init(Logger);
 
         LegacyResourcesDetours.Init();
+        LegacyShaderDetours.Init();
     }
 
     private void OnEnable()
     {
         LegacyResourcesDetours.Enable();
+        LegacyShaderDetours.Enable();
     }
 
     private void OnDisable()
     {
+        LegacyShaderDetours.Disable();
         LegacyResourcesDetours.Disable();
     }
 
     private void OnDestroy()
     {
+        LegacyShaderDetours.Destroy();
         LegacyResourcesDetours.Destroy();
     }
 }
