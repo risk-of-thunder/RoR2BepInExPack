53fbd304e651d0bb595940a923eab200853d237d
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2022-07-25 10:54:55 +0200

    fix code style

--- RoR2BepInExPack/ModCompatibility/FixMultiCorrupt.cs
+++ RoR2BepInExPack/ModCompatibility/FixMultiCorrupt.cs
@@ -1,4 +1,3 @@
-using BepInEx;
 using BepInEx.Configuration;
 using MonoMod.RuntimeDetour;
 using MonoMod.Cil;
@@ -11,18 +10,15 @@ using System.Linq;
 using Mono.Cecil.Cil;
 using System.Runtime.CompilerServices;
 
-using static RoR2.Items.ContagiousItemManager;
-
 namespace RoR2BepInExPack.ModCompatibility;
 
 internal class FixMultiCorrupt
 {
+    private static ILHook _ilHook;
 
-    private static ILHook _ilhook;
-
-    private static ConditionalWeakTable<Inventory,Dictionary<ItemIndex,int>> alternateTracker = new ConditionalWeakTable<Inventory, Dictionary<ItemIndex, int>>();
-    private static Xoroshiro128Plus voidRNG = null;
-    private static ConfigEntry<ContagionPriority> contagionPriority;
+    private static readonly ConditionalWeakTable<Inventory, Dictionary<ItemIndex, int>> _alternateTracker = new();
+    private static Xoroshiro128Plus _voidRNG = null;
+    private static ConfigEntry<ContagionPriority> _contagionPriority;
 
     internal enum ContagionPriority
     {
@@ -33,74 +29,88 @@ internal class FixMultiCorrupt
        Alternate
     }
 
-
     internal static void Init(ConfigFile config)
     {
-        contagionPriority = config.Bind<ContagionPriority>("General","Contagion Priority",ContagionPriority.Random,"Determines behaviour for when multiple results are available for an item transformation.");
-        var ilhookConfig = new ILHookConfig() {ManualApply = true};
-        _ilhook = new ILHook(
-                        typeof(ContagiousItemManager).GetMethod(nameof(ContagiousItemManager.StepInventoryInfection),ReflectionHelper.AllFlags),
-                        FixMultiCorrupt.FixStep,
-                        ref ilhookConfig
-                    );
+        _contagionPriority = config.Bind("General", "Contagion Priority", ContagionPriority.Random, "Determines behaviour for when multiple results are available for an item transformation.");
+        var ilHookConfig = new ILHookConfig() { ManualApply = true };
+        _ilHook = new ILHook(
+                    typeof(ContagiousItemManager).GetMethod(nameof(ContagiousItemManager.StepInventoryInfection),ReflectionHelper.AllFlags),
+                    FixStep,
+                    ref ilHookConfig
+                );
     }
 
     internal static void Enable()
     {
-        _ilhook.Apply();
+        _ilHook.Apply();
     }
 
     internal static void Disable()
     {
-        _ilhook.Undo();
+        _ilHook.Undo();
     }
 
     internal static void Destroy()
     {
-        _ilhook.Free();
+        _ilHook.Free();
     }
 
-    internal static void FixStep(ILContext il)
+    private static void FixStep(ILContext il)
     {
         ILCursor c = new ILCursor(il);
         bool ILFound = c.TryGotoNext(MoveType.After,
-            x => x.MatchLdsfld(typeof(ContagiousItemManager).GetField(nameof(ContagiousItemManager.originalToTransformed),ReflectionHelper.AllFlags)),
+            x => x.MatchLdsfld(typeof(ContagiousItemManager).GetField(nameof(ContagiousItemManager.originalToTransformed), ReflectionHelper.AllFlags)),
             x => x.MatchLdarg(1),
             x => x.MatchLdelemI4());
 
-        if(ILFound){
+        if (ILFound)
+        {
             c.Emit(OpCodes.Pop);
             c.Emit(OpCodes.Ldarg,0);
             c.Emit(OpCodes.Ldarg,1);
-            c.EmitDelegate<Func<Inventory,ItemIndex,ItemIndex>>((inventory,pureItem) =>{
+            c.EmitDelegate<Func<Inventory,ItemIndex,ItemIndex>>((inventory,pureItem) =>
+            {
                 List<ItemIndex> possibilities = inventory.itemAcquisitionOrder.Where(item => ContagiousItemManager.GetOriginalItemIndex(item) == pureItem).ToList();
-                    if(possibilities.Count == 0){
-                      possibilities = ContagiousItemManager.transformationInfos.Where((info) => info.originalItem == pureItem).Select((info) => info.transformedItem).ToList();
-                    }
-                    switch(contagionPriority.Value){
-                        case ContagionPriority.First:
-                          return possibilities.First();
-                        case ContagionPriority.Last:
-                          return possibilities.Last();
-                        case ContagionPriority.Alternate:
-                          var dict = alternateTracker.GetOrCreateValue(inventory);
-                          if(!dict.ContainsKey(pureItem))
-                            dict.Add(pureItem,0);
-                          return possibilities[(dict[pureItem]++)%(possibilities.Count)];
-                        case ContagionPriority.Rarest:
-                          possibilities.Sort((item,item2) => ItemCatalog.GetItemDef(item).tier.CompareTo(ItemCatalog.GetItemDef(item2).tier));
-                          return possibilities.Last();
-                        case ContagionPriority.Random:
-                          if(voidRNG == null){
-                            voidRNG = new Xoroshiro128Plus(RoR2.Run.instance.stageRng.nextUlong);
-                          }
-                          return possibilities[voidRNG.RangeInt(0,possibilities.Count)];
-                        default:      
-                          return ContagiousItemManager.originalToTransformed[(int)pureItem];
-                    }
+
+                if (possibilities.Count == 0)
+                {
+                    possibilities = ContagiousItemManager.transformationInfos.Where((info) => info.originalItem == pureItem).Select((info) => info.transformedItem).ToList();
+                }
+
+                switch (_contagionPriority.Value)
+                {
+                    case ContagionPriority.First:
+                        return possibilities.First();
+
+                    case ContagionPriority.Last:
+                        return possibilities.Last();
+
+                    case ContagionPriority.Alternate:
+                        var dict = _alternateTracker.GetOrCreateValue(inventory);
+
+                        if(!dict.ContainsKey(pureItem))
+                            dict.Add(pureItem, 0);
+
+                        return possibilities[dict[pureItem]++ % possibilities.Count];
+
+                    case ContagionPriority.Rarest:
+                        possibilities.Sort((item, item2) => ItemCatalog.GetItemDef(item).tier.CompareTo(ItemCatalog.GetItemDef(item2).tier));
+                        return possibilities.Last();
+
+                    case ContagionPriority.Random:
+                        if (_voidRNG == null)
+                        {
+                            _voidRNG = new Xoroshiro128Plus(Run.instance.stageRng.nextUlong);
+                        }
+
+                        return possibilities[_voidRNG.RangeInt(0,possibilities.Count)];
+
+                    default:
+                        return ContagiousItemManager.originalToTransformed[(int)pureItem];
+                }
             });
         }
     }
 
-  
+
 }
