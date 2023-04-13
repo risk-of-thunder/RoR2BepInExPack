5c0d43a21a08ae556d0606721493592a3c1fadf2
Quentin E. / iDeath <xiaoxiao921@hotmail.fr>
2023-04-13 02:46:07 +0200

    bump ver

--- RoR2BepInExPack/RoR2BepInExPack.cs
+++ RoR2BepInExPack/RoR2BepInExPack.cs
@@ -12,7 +12,7 @@ public class RoR2BepInExPack : BaseUnityPlugin
 {
     public const string PluginGUID = "___riskofthunder" + "." + PluginName;
     public const string PluginName = "RoR2BepInExPack";
-    public const string PluginVersion = "1.3.0";
+    public const string PluginVersion = "1.4.0";
 
     internal static PluginInfo pluginInfo;
 
