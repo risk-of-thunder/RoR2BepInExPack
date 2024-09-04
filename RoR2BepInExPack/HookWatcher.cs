fbc1f4e47018c89bdd9583582a2c17b41e7e05f8
xiaoxiao921 <837334+xiaoxiao921@users.noreply.github.com>
2024-09-05 01:51:01 +0200

    FixFrameRateDependantLogic: better compat for il hooks

--- RoR2BepInExPack/HookWatcher.cs
+++ RoR2BepInExPack/HookWatcher.cs
@@ -4,6 +4,8 @@ using System.Reflection;
 using MonoMod.Cil;
 using MonoMod.RuntimeDetour;
 using MonoMod.RuntimeDetour.HookGen;
+using RoR2BepInExPack.Reflection;
+using RoR2BepInExPack.VanillaFixes;
 
 namespace RoR2BepInExPack;
 
@@ -42,46 +44,171 @@ internal static class HookWatcher
         ModManager = null;
     }
 
-    private static void LogOnHook(Assembly hookOwner, MethodBase originalMethod, MethodBase _, object __)
-        => LogMethod(originalMethod, hookOwner);
+    private static void LogOnHook(Assembly hookOwner, MethodBase from, MethodBase to, object target)
+        => LogHookAndMaybeRedirect(new() { Kind = HookInfo.HookKind.On, Owner = hookOwner, OriginalManaged = from, HookMethodBase = to });
 
-    private static void LogILHook(Assembly hookOwner, MethodBase originalMethod, ILContext.Manipulator _)
-        => LogMethod(originalMethod, hookOwner);
+    private static void LogILHook(Assembly hookOwner, MethodBase from, ILContext.Manipulator manipulator)
+        => LogHookAndMaybeRedirect(new() { Kind = HookInfo.HookKind.IL, Owner = hookOwner, OriginalManaged = from, HookDelegate = manipulator });
 
-    private static void LogDetour(Assembly hookOwner, MethodBase originalMethod, MethodBase _)
-        => LogMethod(originalMethod, hookOwner);
+    private static void LogDetour(Assembly hookOwner, MethodBase from, MethodBase to)
+        => LogHookAndMaybeRedirect(new() { Kind = HookInfo.HookKind.On, Owner = hookOwner, OriginalManaged = from, HookMethodBase = to });
 
-    private static void LogNativeDetour(Assembly hookOwner, MethodBase originalMethod, IntPtr _, IntPtr __)
-        => LogMethod(originalMethod, hookOwner);
+    private static void LogNativeDetour(Assembly hookOwner, MethodBase originalMethod, IntPtr from, IntPtr to)
+        => LogHookAndMaybeRedirect(new() { Kind = HookInfo.HookKind.Native, Owner = hookOwner, OriginalNative = from, HookIntPtr = to });
 
-    private static bool LogHookAdd(MethodBase originalMethod, Delegate @delegate)
-        => LogMethod(originalMethod, @delegate.Method.Module.Assembly);
-
-    private static bool LogHookModify(MethodBase originalMethod, Delegate @delegate)
-        => LogMethod(originalMethod, @delegate.Method.Module.Assembly);
-
-    private static bool LogHookRemove(MethodBase originalMethod, Delegate @delegate)
-        => LogMethod(originalMethod, @delegate.Method.Module.Assembly, false);
-
-    private static bool LogMethod(MemberInfo originalMethod, Assembly hookOwnerAssembly, bool added = true)
+    private static bool LogHookAdd(MethodBase from, Delegate to)
     {
-        if (originalMethod == null)
+        var info = GetHookInfo(from, to);
+
+        return LogHookAndMaybeRedirect(info);
+    }
+
+    private static bool LogHookModify(MethodBase from, Delegate to)
+    {
+        var info = GetHookInfo(from, to);
+
+        // Seems to be only used by IL Manipulators?
+        return LogHookAndMaybeRedirect(info, "modifier");
+    }
+
+    private static bool LogHookRemove(MethodBase from, Delegate to)
+    {
+        var info = GetHookInfo(from, to);
+
+        return LogHookAndMaybeRedirect(info, "removed");
+    }
+
+    private static HookInfo GetHookInfo(MethodBase from, Delegate to)
+    {
+        var info = new HookInfo()
+        {
+            Owner = to.Method.Module.Assembly,
+            OriginalManaged = from,
+        };
+
+        if (to is ILContext.Manipulator manipulator)
+        {
+            info.Kind = HookInfo.HookKind.IL;
+            info.HookDelegate = manipulator;
+        }
+        else
+        {
+            info.Kind = HookInfo.HookKind.On;
+            info.HookDelegate = to;
+        }
+
+        return info;
+    }
+
+    internal class HookInfo
+    {
+        internal enum HookKind
+        {
+            On,
+            IL,
+            Native,
+        }
+
+        internal HookKind Kind;
+
+        internal Assembly Owner;
+
+        internal MethodBase OriginalManaged;
+        internal IntPtr OriginalNative;
+
+        internal Delegate HookDelegate;
+        internal IntPtr HookIntPtr;
+        internal MethodBase HookMethodBase;
+    }
+
+    internal static bool RedirectFixFrameRateDependantLogicHooks = false;
+    private static bool LogHookAndMaybeRedirect(HookInfo hookInfo, string context = "added")
+    {
+        if (hookInfo.OriginalManaged == null)
         {
             return true;
         }
 
         var hookOwnerDllName = "Not Found";
-        if (hookOwnerAssembly != null)
+        if (hookInfo.Owner != null)
         {
-            // Get the dll name instead of assembly manifest name as this one one could be not correctly set by mod maker.
-            hookOwnerDllName = Path.GetFileName(hookOwnerAssembly.Location);
+            // Get the dll name instead of the assembly manifest name, as the latter may not be correctly defined by the mod creator.
+            hookOwnerDllName = Path.GetFileName(hookInfo.Owner.Location);
         }
 
-        var declaringType = originalMethod.DeclaringType;
-        var name = originalMethod.Name;
-        var identifier = declaringType != null ? $"{declaringType}.{name}" : name;
+        var fromDeclaringType = hookInfo.OriginalManaged.DeclaringType;
+        var fromName = hookInfo.OriginalManaged.Name;
+        var fromIdentifier = fromDeclaringType != null ? $"{fromDeclaringType.FullName}.{fromName}" : fromName;
 
-        Log.Debug($"Hook {(added ? "added" : "removed")} by assembly: {hookOwnerDllName} for: {identifier}");
+        string GetToIdentifier()
+        {
+            if (hookInfo.HookDelegate != null)
+            {
+                var toDeclaringType = hookInfo.HookDelegate.Method?.DeclaringType;
+                var toName = hookInfo.HookDelegate.Method?.Name;
+                return toDeclaringType != null ? $"{toDeclaringType.FullName}.{toName}" : toName;
+            }
+
+            if (hookInfo.HookMethodBase != null)
+            {
+                var toDeclaringType = hookInfo.HookMethodBase.DeclaringType;
+                var toName = hookInfo.HookMethodBase.Name;
+                return toDeclaringType != null ? $"{toDeclaringType.FullName}.{toName}" : toName;
+            }
+
+            return "";
+        }
+
+        Log.Debug($"{hookInfo.Kind}Hook {GetToIdentifier()} {context} by assembly: {hookOwnerDllName} for: {fromIdentifier}");
+
+        if (FixFrameRateDependantLogic.IsFixedEnabled &&
+            RedirectFixFrameRateDependantLogicHooks &&
+            hookInfo.HookDelegate != null && hookInfo.HookDelegate is ILContext.Manipulator manipulator)
+        {
+            // Redirect hooks to the right methods.
+            // Not the right place for this or atleast it should be generalized but no time for this right now
+
+            RedirectFixFrameRateDependantLogicHooks = false;
+
+            try
+            {
+                void LogRedirection()
+                {
+                    Log.Info($"Redirecting {hookInfo.HookDelegate.Method?.DeclaringType?.FullName}.{hookInfo.HookDelegate.Method?.Name}" +
+                        $" IL Hook to {fromIdentifier} because the {nameof(FixFrameRateDependantLogic)} fix is enabled.");
+                }
+
+                if (fromIdentifier == $"RoR2.{nameof(RoR2.PlayerCharacterMasterController)}.Update")
+                {
+                    LogRedirection();
+
+                    new ILHook(
+                        typeof(FixFrameRateDependantLogic).
+                            GetMethod(nameof(FixFrameRateDependantLogic.PlayerCharacterMasterController_Update),
+                            ReflectionHelper.AllFlags),
+                        manipulator
+                    );
+                }
+                else if (fromIdentifier == $"RoR2.{nameof(RoR2.PlayerCharacterMasterController)}.FixedUpdate")
+                {
+                    LogRedirection();
+
+                    new ILHook(
+                        typeof(FixFrameRateDependantLogic).
+                            GetMethod(nameof(FixFrameRateDependantLogic.PlayerCharacterMasterController_FixedUpdate),
+                            ReflectionHelper.AllFlags),
+                        manipulator
+                    );
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Warning(e);
+            }
+
+            RedirectFixFrameRateDependantLogicHooks = true;
+        }
+
         return true;
     }
 }
--- RoR2BepInExPack/VanillaFixes/FixFrameRateDependantLogic.cs
+++ RoR2BepInExPack/VanillaFixes/FixFrameRateDependantLogic.cs
@@ -14,7 +14,7 @@ namespace RoR2BepInExPack.VanillaFixes;
 /// <summary>
 /// Fix that attempts to revert the changes made by Gearbox, which made most of the game's logic dependent on frame rate.
 /// </summary>
-internal class FixFrameRateDependantLogic
+public class FixFrameRateDependantLogic
 {
     private static ConfigEntry<bool> _isFixEnabled;
 
@@ -25,6 +25,11 @@ internal class FixFrameRateDependantLogic
     private static ILHook _updateHookPCMC;
 
     private static ILHook _healthBarUpdateHook;
+
+    /// <summary>
+    /// Whether or not the fix is enabled.
+    /// </summary>
+    public static bool IsFixedEnabled => _isFixEnabled != null && _isFixEnabled.Value;
 
     internal static void Init(ConfigFile config)
     {
@@ -40,8 +45,10 @@ internal class FixFrameRateDependantLogic
                 return;
             }
 
+            const string FixedUpdateMethodName = "FixedUpdate";
+
             _fixedUpdateHook = new ILHook(
-                typeof(HG.MonoBehaviourManager).GetMethod("FixedUpdate", ReflectionHelper.AllFlags),
+                typeof(HG.MonoBehaviourManager).GetMethod(FixedUpdateMethodName, ReflectionHelper.AllFlags),
                 FixedUpdateHook);
 
             _healthBarUpdateHook = new ILHook(
@@ -53,12 +60,14 @@ internal class FixFrameRateDependantLogic
                 UpdateHook);
 
             _fixedUpdateHookPCMC = new ILHook(
-                typeof(PlayerCharacterMasterController).GetMethod("FixedUpdate", ReflectionHelper.AllFlags),
-                FixedUpdatePCMCHook);
+                typeof(PlayerCharacterMasterController).GetMethod(FixedUpdateMethodName, ReflectionHelper.AllFlags),
+                PlayerCharacterMasterController_FixedUpdate_ILManipulator);
 
             _updateHookPCMC = new ILHook(
                 typeof(PlayerCharacterMasterController).GetMethod(nameof(PlayerCharacterMasterController.Update), ReflectionHelper.AllFlags),
-                UpdatePCMCHook);
+                PlayerCharacterMasterController_Update_ILManipulator);
+
+            HookWatcher.RedirectFixFrameRateDependantLogicHooks = true;
         }
         catch (Exception e)
         {
@@ -78,39 +87,42 @@ internal class FixFrameRateDependantLogic
         c.Emit(OpCodes.Ret);
     }
 
-    private static void UpdateDefault(MonoBehaviourManager self)
+    private static class FixedMonoBehaviourManager
     {
-        for (int i = 0; i < self.registeredBehaviours.Count; i++)
+        internal static void UpdateDefault(MonoBehaviourManager self)
         {
-            IManagedMonoBehaviour managedMonoBehaviour = self.registeredBehaviours[i];
-            if (managedMonoBehaviour.IsEnabled())
+            for (int i = 0; i < self.registeredBehaviours.Count; i++)
             {
-                try
+                IManagedMonoBehaviour managedMonoBehaviour = self.registeredBehaviours[i];
+                if (managedMonoBehaviour.IsEnabled())
                 {
-                    managedMonoBehaviour.ManagedUpdate();
-                }
-                catch (Exception exception)
-                {
-                    Debug.LogException(exception);
+                    try
+                    {
+                        managedMonoBehaviour.ManagedUpdate();
+                    }
+                    catch (Exception exception)
+                    {
+                        Debug.LogException(exception);
+                    }
                 }
             }
         }
-    }
 
-    private static void FixedUpdateDefault(MonoBehaviourManager self)
-    {
-        for (int i = 0; i < self.registeredBehaviours.Count; i++)
+        internal static void FixedUpdateDefault(MonoBehaviourManager self)
         {
-            IManagedMonoBehaviour managedMonoBehaviour = self.registeredBehaviours[i];
-            if (managedMonoBehaviour.IsEnabled() && Time.deltaTime > 0f)
+            for (int i = 0; i < self.registeredBehaviours.Count; i++)
             {
-                try
+                IManagedMonoBehaviour managedMonoBehaviour = self.registeredBehaviours[i];
+                if (managedMonoBehaviour.IsEnabled() && Time.deltaTime > 0f)
                 {
-                    managedMonoBehaviour.ManagedFixedUpdate(Time.deltaTime);
-                }
-                catch (Exception exception)
-                {
-                    Debug.LogException(exception);
+                    try
+                    {
+                        managedMonoBehaviour.ManagedFixedUpdate(Time.deltaTime);
+                    }
+                    catch (Exception exception)
+                    {
+                        Debug.LogException(exception);
+                    }
                 }
             }
         }
@@ -121,7 +133,7 @@ internal class FixFrameRateDependantLogic
         var c = new ILCursor(il);
 
         c.Emit(OpCodes.Ldarg_0);
-        c.EmitDelegate(UpdateDefault);
+        c.EmitDelegate(FixedMonoBehaviourManager.UpdateDefault);
         c.Emit(OpCodes.Ret);
     }
 
@@ -130,140 +142,144 @@ internal class FixFrameRateDependantLogic
         var c = new ILCursor(il);
 
         c.Emit(OpCodes.Ldarg_0);
-        c.EmitDelegate(FixedUpdateDefault);
+        c.EmitDelegate(FixedMonoBehaviourManager.FixedUpdateDefault);
         c.Emit(OpCodes.Ret);
     }
 
-    private static void FixedUpdatePCMCHook(ILContext il)
+    private static void PlayerCharacterMasterController_FixedUpdate_ILManipulator(ILContext il)
     {
         var c = new ILCursor(il);
 
         c.Emit(OpCodes.Ldarg_0);
-        c.EmitDelegate((PlayerCharacterMasterController self) =>
+        c.EmitDelegate(PlayerCharacterMasterController_FixedUpdate);
+        c.Emit(OpCodes.Ret);
+    }
+
+    internal static void PlayerCharacterMasterController_FixedUpdate(PlayerCharacterMasterController self)
+    {
+        if (!self.hasEffectiveAuthority || !self.bodyInputs)
         {
-            if (!self.hasEffectiveAuthority || !self.bodyInputs)
+            return;
+        }
+        bool newState = false;
+        bool newState2 = false;
+        bool newState3 = false;
+        bool newState4 = false;
+        bool newState5 = false;
+        bool newState6 = false;
+        bool newState7 = false;
+        bool newState8 = false;
+        bool newState9 = false;
+        bool flag = false;
+        if (PlayerCharacterMasterController.CanSendBodyInput(self.networkUser, out var localUser, out var inputPlayer, out var cameraRigController, out var onlyAllowMovement))
+        {
+            bool flag2 = false;
+            flag2 = self.body.isSprinting;
+            if (self.sprintInputPressReceived)
             {
-                return;
+                self.sprintInputPressReceived = false;
+                flag2 = !flag2;
             }
-            bool newState = false;
-            bool newState2 = false;
-            bool newState3 = false;
-            bool newState4 = false;
-            bool newState5 = false;
-            bool newState6 = false;
-            bool newState7 = false;
-            bool newState8 = false;
-            bool newState9 = false;
-            bool flag = false;
-            if (PlayerCharacterMasterController.CanSendBodyInput(self.networkUser, out var localUser, out var inputPlayer, out var cameraRigController, out var onlyAllowMovement))
+            if (flag2)
             {
-                bool flag2 = false;
-                flag2 = self.body.isSprinting;
-                if (self.sprintInputPressReceived)
+                Vector3 aimDirection = self.bodyInputs.aimDirection;
+                aimDirection.y = 0f;
+                aimDirection.Normalize();
+                Vector3 moveVector = self.bodyInputs.moveVector;
+                moveVector.y = 0f;
+                moveVector.Normalize();
+                if ((self.body.bodyFlags & CharacterBody.BodyFlags.SprintAnyDirection) == 0 && Vector3.Dot(aimDirection, moveVector) < PlayerCharacterMasterController.sprintMinAimMoveDot)
                 {
-                    self.sprintInputPressReceived = false;
-                    flag2 = !flag2;
+                    flag2 = false;
                 }
-                if (flag2)
-                {
-                    Vector3 aimDirection = self.bodyInputs.aimDirection;
-                    aimDirection.y = 0f;
-                    aimDirection.Normalize();
-                    Vector3 moveVector = self.bodyInputs.moveVector;
-                    moveVector.y = 0f;
-                    moveVector.Normalize();
-                    if ((self.body.bodyFlags & CharacterBody.BodyFlags.SprintAnyDirection) == 0 && Vector3.Dot(aimDirection, moveVector) < PlayerCharacterMasterController.sprintMinAimMoveDot)
-                    {
-                        flag2 = false;
-                    }
-                }
-                if (!onlyAllowMovement)
-                {
-                    newState = inputPlayer.GetButton(7);
-                    newState2 = inputPlayer.GetButton(8);
-                    newState3 = inputPlayer.GetButton(9);
-                    newState4 = inputPlayer.GetButton(10);
-                    newState5 = inputPlayer.GetButton(5);
-                    newState6 = inputPlayer.GetButton(4);
-                    newState7 = flag2;
-                    newState8 = inputPlayer.GetButton(6);
-                    newState9 = inputPlayer.GetButton(28);
-                }
+            }
+            if (!onlyAllowMovement)
+            {
+                newState = inputPlayer.GetButton(7);
+                newState2 = inputPlayer.GetButton(8);
+                newState3 = inputPlayer.GetButton(9);
+                newState4 = inputPlayer.GetButton(10);
+                newState5 = inputPlayer.GetButton(5);
+                newState6 = inputPlayer.GetButton(4);
+                newState7 = flag2;
+                newState8 = inputPlayer.GetButton(6);
+                newState9 = inputPlayer.GetButton(28);
+            }
+        }
+        else
+        {
+            flag = true;
+        }
+        self.bodyInputs.skill1.PushState(newState);
+        self.bodyInputs.skill2.PushState(newState2);
+        self.bodyInputs.skill3.PushState(newState3);
+        self.bodyInputs.skill4.PushState(newState4);
+        self.bodyInputs.interact.PushState(newState5);
+        self.bodyInputs.jump.PushState(newState6);
+        self.bodyInputs.jump.hasPressBeenClaimed = self.wasClaimed;
+        self.wasClaimed = flag;
+        self.bodyInputs.sprint.PushState(newState7);
+        self.bodyInputs.activateEquipment.PushState(newState8);
+        self.bodyInputs.ping.PushState(newState9);
+        self.CheckPinging();
+    }
+
+    private static void PlayerCharacterMasterController_Update_ILManipulator(ILContext il)
+    {
+        var c = new ILCursor(il);
+
+        c.Emit(OpCodes.Ldarg_0);
+        c.EmitDelegate(PlayerCharacterMasterController_Update);
+        c.Emit(OpCodes.Ret);
+    }
+
+    internal static void PlayerCharacterMasterController_Update(PlayerCharacterMasterController self)
+    {
+        if (!self.hasEffectiveAuthority)
+        {
+            return;
+        }
+        GameObject bodyObject = self.master.GetBodyObject();
+        if (self.previousBodyObject == null)
+        {
+            self.previousBodyObject = bodyObject;
+            self.SetBody(bodyObject);
+        }
+        else if (self.previousBodyObject != bodyObject)
+        {
+            self.SetBody(bodyObject);
+            self.previousBodyObject = bodyObject;
+        }
+        if (!self.bodyInputs)
+        {
+            return;
+        }
+        Vector3 moveVector = Vector3.zero;
+        Vector3 aimDirection = self.bodyInputs.aimDirection;
+        if (PlayerCharacterMasterController.CanSendBodyInput(self.networkUser, out var localUser, out var inputPlayer, out var cameraRigController, out var onlyAllowMovement))
+        {
+            Transform transform = cameraRigController.transform;
+            self.sprintInputPressReceived |= inputPlayer.GetButtonDown(18);
+            Vector2 vector = new Vector2(inputPlayer.GetAxis(0), inputPlayer.GetAxis(1));
+            Vector2 vector2 = new Vector2(inputPlayer.GetAxis(12), inputPlayer.GetAxis(13));
+            self.bodyInputs.SetRawMoveStates(vector + vector2);
+            float sqrMagnitude = vector.sqrMagnitude;
+            if (sqrMagnitude > 1f)
+            {
+                vector /= Mathf.Sqrt(sqrMagnitude);
+            }
+            if (self.bodyIsFlier)
+            {
+                moveVector = transform.right * vector.x + transform.forward * vector.y;
             }
             else
             {
-                flag = true;
+                float y = transform.eulerAngles.y;
+                moveVector = Quaternion.Euler(0f, y, 0f) * new Vector3(vector.x, 0f, vector.y);
             }
-            self.bodyInputs.skill1.PushState(newState);
-            self.bodyInputs.skill2.PushState(newState2);
-            self.bodyInputs.skill3.PushState(newState3);
-            self.bodyInputs.skill4.PushState(newState4);
-            self.bodyInputs.interact.PushState(newState5);
-            self.bodyInputs.jump.PushState(newState6);
-            self.bodyInputs.jump.hasPressBeenClaimed = self.wasClaimed;
-            self.wasClaimed = flag;
-            self.bodyInputs.sprint.PushState(newState7);
-            self.bodyInputs.activateEquipment.PushState(newState8);
-            self.bodyInputs.ping.PushState(newState9);
-            self.CheckPinging();
-        });
-        c.Emit(OpCodes.Ret);
-    }
-
-    private static void UpdatePCMCHook(ILContext il)
-    {
-        var c = new ILCursor(il);
-
-        c.Emit(OpCodes.Ldarg_0);
-        c.EmitDelegate((PlayerCharacterMasterController self) =>
-        {
-            if (!self.hasEffectiveAuthority)
-            {
-                return;
-            }
-            GameObject bodyObject = self.master.GetBodyObject();
-            if (self.previousBodyObject == null)
-            {
-                self.previousBodyObject = bodyObject;
-                self.SetBody(bodyObject);
-            }
-            else if (self.previousBodyObject != bodyObject)
-            {
-                self.SetBody(bodyObject);
-                self.previousBodyObject = bodyObject;
-            }
-            if (!self.bodyInputs)
-            {
-                return;
-            }
-            Vector3 moveVector = Vector3.zero;
-            Vector3 aimDirection = self.bodyInputs.aimDirection;
-            if (PlayerCharacterMasterController.CanSendBodyInput(self.networkUser, out var localUser, out var inputPlayer, out var cameraRigController, out var onlyAllowMovement))
-            {
-                Transform transform = cameraRigController.transform;
-                self.sprintInputPressReceived |= inputPlayer.GetButtonDown(18);
-                Vector2 vector = new Vector2(inputPlayer.GetAxis(0), inputPlayer.GetAxis(1));
-                Vector2 vector2 = new Vector2(inputPlayer.GetAxis(12), inputPlayer.GetAxis(13));
-                self.bodyInputs.SetRawMoveStates(vector + vector2);
-                float sqrMagnitude = vector.sqrMagnitude;
-                if (sqrMagnitude > 1f)
-                {
-                    vector /= Mathf.Sqrt(sqrMagnitude);
-                }
-                if (self.bodyIsFlier)
-                {
-                    moveVector = transform.right * vector.x + transform.forward * vector.y;
-                }
-                else
-                {
-                    float y = transform.eulerAngles.y;
-                    moveVector = Quaternion.Euler(0f, y, 0f) * new Vector3(vector.x, 0f, vector.y);
-                }
-                aimDirection = (cameraRigController.crosshairWorldPosition - self.bodyInputs.aimOrigin).normalized;
-            }
-            self.bodyInputs.moveVector = moveVector;
-            self.bodyInputs.aimDirection = aimDirection;
-        });
-        c.Emit(OpCodes.Ret);
+            aimDirection = (cameraRigController.crosshairWorldPosition - self.bodyInputs.aimOrigin).normalized;
+        }
+        self.bodyInputs.moveVector = moveVector;
+        self.bodyInputs.aimDirection = aimDirection;
     }
 }
