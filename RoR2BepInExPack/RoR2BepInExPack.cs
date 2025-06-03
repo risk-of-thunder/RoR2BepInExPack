a728838db860521b7fe753e1545279bec220c8ee
xiaoxiao921 <837334+xiaoxiao921@users.noreply.github.com>
2025-06-04 01:41:44 +0200

    bump ver

--- RoR2BepInExPack/RoR2BepInExPack.cs
+++ RoR2BepInExPack/RoR2BepInExPack.cs
@@ -15,7 +15,7 @@ public class RoR2BepInExPack : BaseUnityPlugin
 {
     public const string PluginGUID = "___riskofthunder" + "." + PluginName;
     public const string PluginName = "RoR2BepInExPack";
-    public const string PluginVersion = "1.28.0";
+    public const string PluginVersion = "1.29.0";
 
     private void Awake()
     {
