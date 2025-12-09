80d30fc0279160cc8ae9531f7a2f1ac3634b462d
Quentin <837334+xiaoxiao921@users.noreply.github.com>
2025-12-09 22:53:40 +0100

    fix lrapi_returns.json missing for 1.4.1

---
+++ src/VanillaFixes/FixLRAPIReturns.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using BepInEx;
+using UnityEngine;
+using Path = System.IO.Path;
+
+namespace RoR2BepInExPack.VanillaFixes;
+
+internal class FixLRAPIReturns
+{
+    internal static void Init(PluginInfo pluginInfo)
+    {
+        try
+        {
+            const string lrapiJsonFileName = "lrapi_returns.json";
+
+            var destination = Path.Combine(Application.streamingAssetsPath, lrapiJsonFileName);
+            if (File.Exists(destination))
+            {
+                Log.Info($"{lrapiJsonFileName} already exists in StreamingAssets, skipping fix.");
+                return;
+            }
+
+            var source = Path.Combine(Directory.GetParent(pluginInfo.Location).FullName, lrapiJsonFileName);
+
+            File.Copy(source, destination);
+        }
+        catch (Exception e)
+        {
+            Log.Error(e);
+        }
+    }
+}
